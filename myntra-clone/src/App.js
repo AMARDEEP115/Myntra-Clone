f104bc19b9b5e89b86c7773cc142d1387e3370ed
AMAR DEEP <amar1998deep@gmail.com>
2022-12-13 15:05:04 +0530

    react app myntra

---
+++ myntra-clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainRoutes from './RouterPages/MainRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoutes />
+    </div>
+  );
+}
+
+export default App;
